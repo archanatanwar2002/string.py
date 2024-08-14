d8aa442f765487bb27ea302a3b2bfb234b4e48b4
archanatanwar2002 <archanatanwar2002@gmail.com>
2024-08-14 19:18:23 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,12 @@
+#name = input("enter yourname :")
+#print(name)
+light = "yellow"
+if(light == "red"):
+    print("stop")
+elif(light== "green"):
+    print("go")
+elif(light == "yellow"):
+    print("wait")
+
+    
+
